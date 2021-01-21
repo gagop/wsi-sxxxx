12bc558e743ebf42f824c882f7978cd6e115bb3b
Piotr Gago <gagopiotr@gmail.com>
2021-01-21 09:59:26 +0100

    Modified something

--- Program.java
+++ Program.java
@@ -1,7 +1,7 @@
 public class Program
 {
 	public static void main(String[] args){
-		System.out.println("Hello world and something else...");
+		System.out.println("Hello world and something else... AAA");
 	}
 
 }
